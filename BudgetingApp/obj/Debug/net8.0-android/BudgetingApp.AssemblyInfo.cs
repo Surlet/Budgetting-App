2461b36470e69a5f09a05fef7a9df6af8ff29f1f
Antoine <antoinepbglejeune@gmail.com>
2025-12-04 00:04:59 +0100

    Validate files

--- BudgetingApp/obj/Debug/net8.0-android/BudgetingApp.AssemblyInfo.cs
+++ BudgetingApp/obj/Debug/net8.0-android/BudgetingApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+10ddf4e2205fa78348f32986f82c30d83586c443")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
