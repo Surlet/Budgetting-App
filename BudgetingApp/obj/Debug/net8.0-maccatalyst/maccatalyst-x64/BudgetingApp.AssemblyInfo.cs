89bea335dfaf06c5fef5c18d92bc7670b2f422ed
Antoine <antoinepbglejeune@gmail.com>
2025-12-04 13:23:31 +0100

    Add SettingsPage view with first layout

--- BudgetingApp/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/BudgetingApp.AssemblyInfo.cs
+++ BudgetingApp/obj/Debug/net8.0-maccatalyst/maccatalyst-x64/BudgetingApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+10ddf4e2205fa78348f32986f82c30d83586c443")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+02a7b96480fad66ec90d90c2519a20313f455a32")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
