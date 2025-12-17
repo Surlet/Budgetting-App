f4c94f800b809d8de8777fe166cec9e784340d8d
Antoine <antoinepbglejeune@gmail.com>
2025-12-17 13:49:28 +0100

    Recuperate correct version

--- BudgetingApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BudgetingApp.AssemblyInfo.cs
+++ BudgetingApp/obj/Debug/net8.0-windows10.0.19041.0/win10-x64/BudgetingApp.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+a309f2efa73542ad151125dea1846ad920fa4358")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+491a1dc5fe9f6e3d45a7adadf28d862698ee5b63")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetingApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
